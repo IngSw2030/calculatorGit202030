449ed8bab52edfc5be05ee61751a25314167e836
Jorge <salgadojorge@javeriana.edu.co>
2020-09-17 17:52:41 -0500

    Calculadora Jorge Salgado

--- src/co/edu/javeriana/gitsample/Authors.java
+++ src/co/edu/javeriana/gitsample/Authors.java
@@ -1,18 +1,18 @@
 package co.edu.javeriana.gitsample;
 
 public class Authors {
-	public static final String CAPA="Carlos Parra";
-	public static final String AA="Another Author";
-	public static final String SAA="Still Another Author...";
-	public static final String DAHE="Daniel Hernandez";
-	public static final String JHVG="Jairo Hernan Vanegas Garcia";
-	public static final String GGC="Gabriel Gomez Corredor";
-    public static final String ADGB="Andres David Gomez Bautista";
-	public static final String JPMP= "Juan Pablo Mendez Perdomo";
-	public static final String DABM= "Diego Andres Burgos Melo";
-	public static final String JCSJ="Juan Carlos Su�rez Jaimes";
-	public static final String JMDL="Juan Manuel Duarte Lopez";
-	public static final String SACH="Santiago Andres Caroprese Hidalgo";
+	public static final String CAPA = "Carlos Parra";
+	public static final String AA = "Another Author";
+	public static final String SAA = "Still Another Author...";
+	public static final String DAHE = "Daniel Hernandez";
+	public static final String JHVG = "Jairo Hernan Vanegas Garcia";
+	public static final String GGC = "Gabriel Gomez Corredor";
+	public static final String ADGB = "Andres David Gomez Bautista";
+	public static final String JPMP = "Juan Pablo Mendez Perdomo";
+	public static final String DABM = "Diego Andres Burgos Melo";
+	public static final String JCSJ = "Juan Carlos Su�rez Jaimes";
+	public static final String JMDL = "Juan Manuel Duarte Lopez";
+	public static final String SACH = "Santiago Andres Caroprese Hidalgo";
 	public static final String SGO = "Stiven Gonzalez Olaya";
 	public static final String JJAO = "Juan José Afanador Ochoa";
 	public static final String JACS = "Julian Arturo Calle";
@@ -21,5 +21,6 @@ public class Authors {
 	public static final String JJGM = "John Jairo Gonzalez Martinez";
 	public static final String LSLL = "Leonel Steven Londono Londono";
 	public static final String EAGS = "Erika Alejandra Gonzalez Solano";
-	
+	public static final String JRST = "Jorge Rodrigo Salgado Tello";
+
 }
