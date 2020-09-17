26372d4dadaa07bfbfe87b860877c27651ed8840
gomezan <36377557+gomezan@users.noreply.github.com>
2020-09-17 00:14:07 -0500

    Esta es la tarea de git

--- src/co/edu/javeriana/gitsample/CalculadoraADGB.java
+++ src/co/edu/javeriana/gitsample/CalculadoraADGB.java
@@ -1,30 +1,30 @@
 package co.edu.javeriana.gitsample;
 
-public class CalculadoraGGC implements ICalculator {
+public class CalculadoraADGB implements ICalculator {
 
 
 	public double add(double a, double b) {
-		return a + b;
+		return a+b;
 	}
 
 
 	public double multiply(double a, double b) {
-		return a * b;
+		return a*b;
 	}
 
 
 	public double divide(double a, double b) {
-		return a / b;
+		return a/b;
 	}
 
 
 	public double substract(double a, double b) {
-		return a - b;
+		return a-b;
 	}
 
 
 	public String about() {
-		String linea="Calculator implementend by <<Authors.GGC>>";
+		String linea="Calculator implementend by << " + Authors.ADGB + ">>";
 		return linea;
 	}
 
