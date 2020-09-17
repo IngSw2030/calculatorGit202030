0676d7b361d4e2a5b1e47de9bac93c69408beb78
Gabriel Niño <nino.gandres@javeriana.edu.co>
2020-09-17 15:19:48 -0500

    Modificacion Gabriel Niño

--- src/co/edu/javeriana/gitsample/Authors.java
+++ src/co/edu/javeriana/gitsample/Authors.java
@@ -15,5 +15,6 @@ public class Authors {
 	public static final String SACH="Santiago Andres Caroprese Hidalgo";
 	public static final String SGO = "Stiven Gonzalez Olaya";
 	public static final String JACS = "Julian Arturo Calle";
+	public static final String GANC = "Gabriel Andres Nino Carvajal"
 	
 }
