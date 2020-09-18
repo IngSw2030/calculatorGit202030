2f78df78938cc485877941c7672f6e72aa8f8244
juan Angarita <juseanto@gmail.com>
2020-09-17 20:26:11 -0500

    agregando calculadora por JSAT

--- src/co/edu/javeriana/gitsample/Authors.java
+++ src/co/edu/javeriana/gitsample/Authors.java
@@ -1,22 +1,8 @@
 package co.edu.javeriana.gitsample;
 
 public class Authors {
-<<<<<<< HEAD
-	public static final String CAPA="Carlos Parra";
-	public static final String AA="Another Author";
-	public static final String SAA="Still Another Author...";
-	public static final String DAHE="Daniel Hernandez";
-	public static final String JHVG="Jairo Hernan Vanegas Garcia";
-	public static final String GGC="Gabriel Gomez Corredor";
-    	public static final String ADGB="Andres David Gomez Bautista";
-	public static final String JPMP= "Juan Pablo Mendez Perdomo";
-	public static final String DABM= "Diego Andres Burgos Melo";
-	public static final String JCSJ="Juan Carlos Su�rez Jaimes";
-	public static final String JMDL="Juan Manuel Duarte Lopez";
-	public static final String SACH="Santiago Andres Caroprese Hidalgo";
-=======
-	public static final String CAPA = "Carlos Parra";
 	public static final String AA = "Another Author";
+	public static final String CAPA = "Carlos Parra";
 	public static final String SAA = "Still Another Author...";
 	public static final String DAHE = "Daniel Hernandez";
 	public static final String JHVG = "Jairo Hernan Vanegas Garcia";
@@ -27,7 +13,6 @@ public class Authors {
 	public static final String JCSJ = "Juan Carlos Su�rez Jaimes";
 	public static final String JMDL = "Juan Manuel Duarte Lopez";
 	public static final String SACH = "Santiago Andres Caroprese Hidalgo";
->>>>>>> 449ed8bab52edfc5be05ee61751a25314167e836
 	public static final String SGO = "Stiven Gonzalez Olaya";
 	public static final String JJAO = "Juan José Afanador Ochoa";
 	public static final String JACS = "Julian Arturo Calle";
@@ -35,12 +20,8 @@ public class Authors {
 	public static final String SRD = "Santiago Roa Dueñas";
 	public static final String JJGM = "John Jairo Gonzalez Martinez";
 	public static final String LSLL = "Leonel Steven Londono Londono";
-<<<<<<< HEAD
 	public static final String JMY= "Jenifer Medina Yepez";
-	
-=======
 	public static final String EAGS = "Erika Alejandra Gonzalez Solano";
 	public static final String JRST = "Jorge Rodrigo Salgado Tello";
-
->>>>>>> 449ed8bab52edfc5be05ee61751a25314167e836
+	public static final String JSAT = "Juan Sebastian Angarita Torres";
 }
